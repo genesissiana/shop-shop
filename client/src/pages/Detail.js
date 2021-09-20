f64f8dac4d011167f9997ca6047b21f58ad73088
genesissiana <perezgenesisa@gmail.com>
2021-09-19 22:10:25 -0700

    adding items to cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -4,7 +4,13 @@ import { useQuery } from '@apollo/client';
 
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
+
 import spinner from '../assets/spinner.gif';
 import Cart from '../components/Cart';
 
@@ -18,6 +24,13 @@ const [currentProduct, setCurrentProduct] = useState({})
 const { loading, data } = useQuery(QUERY_PRODUCTS);
 
 const { products } = state;
+
+const addToCart = () => {
+  dispatch({
+    type: ADD_TO_CART,
+    product: { ...currentProduct, purchaseQuantity: 1 }
+  });
+};
 
 useEffect(() => {
   if (products.length) {
@@ -42,7 +55,7 @@ useEffect(() => {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to cart</button>
             <button>Remove from Cart</button>
           </p>
 
