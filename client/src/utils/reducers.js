62be00598c5658b355fb135bd001c748b420b819
genesissiana <perezgenesisa@gmail.com>
2021-09-19 20:47:59 -0700

    passed all cart test

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -41,8 +41,43 @@ import {
   return {
     ...state,
     cart: [...state.cart, ...action.products],
+  };
+  case REMOVE_FROM_CART:
+  let newState = state.cart.filter(product => {
+    return product._id !== action._id;
+  });
+
+  return {
+    ...state,
+    cartOpen: newState.length > 0,
+    cart: newState
+  
+  };
+  
+  case UPDATE_CART_QUANTITY:
+  return {
+    ...state,
+    cartOpen: true,
+    cart: state.cart.map(product => {
+      if (action._id === product._id) {
+        product.purchaseQuantity = action.purchaseQuantity;
+      }
+      return product;
+    })
+  };
+
+  case CLEAR_CART:
+  return {
+    ...state,
+    cartOpen: false,
+    cart: []
+  };
 
 
+  case TOGGLE_CART:
+  return {
+    ...state,
+    cartOpen: !state.cartOpen
   };
   
       default:
